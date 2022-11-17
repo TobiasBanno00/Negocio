f00d118e18c2f230e13c014d07f19bb077b22c88
TobiasBanno <tobiasbanno@gmail.com>
2022-11-17 20:59:31 -0300

    subida de ultimos cambios echos

--- ClienteNegocio/src/component/hooks/InfoAuto.js
+++ ClienteNegocio/src/component/hooks/InfoAuto.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React , {useState}from 'react'
 import "../../style/infoAuto.css"
+import {AiOutlineSearch} from "react-icons/ai";
 
 function InfoAuto() {
+
+    const [patente,setPatente] = useState('')
+    const handleChange = e => {
+       
+        setPatente(e.target.value) 
+        console.log(patente)
+      }
+
   return (
     <div className='contenedor-info'>
-        <h2> Ingrese el número de patente de su vehículo</h2>
-        <input className="entradaBusqueda"  name='patente' type="text" />
+        <h4 className='info-titulo'> Ingrese el número de patente de su vehículo</h4>
+        <div className='contenedor-busqueda'>
+            <AiOutlineSearch/>
+            <input  onChange={handleChange} className="entradaBusqueda"  name='patente' type="text" />
+            <button type='submit' className='info-boton'>Buscar</button>
+        </div>
+        
+        {/* PONER ESTO EN COMPONENTE  
+    <table id="customers">       
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Apellido</th>
+      
+            </tr>
+        </thead>
+    </table> */}
 
     </div>
   )
